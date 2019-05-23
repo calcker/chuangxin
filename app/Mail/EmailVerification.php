687001396a0f65715bc4456c685349b9b91f2fa6
vagrant <vagrant@homestead>
2019-05-23 00:38:50 +0000

    2019-05-23

--- app/Mail/EmailVerification.php
+++ app/Mail/EmailVerification.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class EmailAccountVerification extends Mailable
+class EmailVerification extends Mailable
 {
     use Queueable, SerializesModels;
 
