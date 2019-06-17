6fc28b84b4219b5cd746ee68314cf28f58293c36
vagrant <vagrant@homestead>
2019-06-17 00:37:31 +0000

    2019-06-17

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.default')
 
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
-    </p>
-    <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
-    </p>
+<div class="row">
+  <div class="col-md-2">
+    <div class="side-nav list-group">
+      <a href="#" class="list-group-item list-group-item-action active"><strong>首页</strong></a>
+      <a href="#" class="list-group-item list-group-item-action"><strong>作品</strong></a>
+      <a href="#" class="list-group-item list-group-item-action"><strong>团队</strong></a>
+      <a href="#" class="list-group-item list-group-item-action"><strong>话题</strong></a>
+      <a href="#" class="list-group-item list-group-item-action"><strong>合作</strong></a>
+      <a href="#" class="list-group-item list-group-item-action"><strong>城市</strong></a>
+    </div>
   </div>
+  <div class="col-md-7">
+    <h5>动态</h5>
+  </div>
+  <div class="col-md-3">
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="发现..." aria-label="search" aria-describedby="basic-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="button">搜</button>
+      </div>
+    </div>
+  </div>
+</div>
 @stop
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,13 +1,16 @@
-<nav class="navbar navbar-expand-md fixed-top bg-light">
-  	<div class="container">
-    	<a class="navbar-brand" href="/"></a>
-    	<ul class="navbar-nav justify-content-end">
-      		@if (Auth::check())
-            <li class="nav-item"><a class="nav-link" href="/logout">退出</a></li>
-      		@else
-      			<li class="nav-item"><a class="nav-link" href="/login">登录</a></li>
-      			<li class="nav-item" ><a class="nav-link" href="/register/person">注册</a></li>
-  			@endif
-   		</ul>
-  	</div>
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <img src="/images/logo.png" alt="logo">
+      <strong>创新之城</strong>
+    </a>
+    <ul class="navbar-nav justify-content-end">
+      @if (Auth::check())
+      	<li class="nav-item"><a class="nav-link" href="/logout"><strong>退出</strong></a></li>
+      @else
+      	<li class="nav-item"><a class="nav-link" href="/login"><strong>登录</strong></a></li>
+   		<li class="nav-item" ><a class="nav-link" href="/register/person"><strong>注册</strong></a></li>
+  	  @endif
+   	</ul>
+  </div>
 </nav>
