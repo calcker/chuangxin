a26c466ee4121800e16d634611e70f1223f803ce
vagrant <vagrant@homestead>
2019-07-04 17:39:28 +0000

    2019-07-05

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,10 @@
     <title>创新之城 - 我们一起改变世界</title>
     <link href="/images/logo.png" rel="shortcut icon">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!--[if lt IE 9]>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
   <body>
     <div id="app">
@@ -13,6 +17,10 @@
         @include('layouts._footer')
       </div>
     </div>
+     <!--[if lt IE 9]>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
   <!--
    <script type="text/javascript" src="@yield('js')"></script>
   -->
